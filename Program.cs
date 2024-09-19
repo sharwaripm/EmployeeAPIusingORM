127ed29a472bd0fa348efe4705f26601abee1cd4
sharwaripm <sharprak31@gmail.com>
2024-09-19 17:11:18 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+using EmployeeWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add the CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+// Configure the database context with SQL Server
+builder.Services.AddDbContext<EmployeeDatabaseContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MyDBConnection"));
+});
+
+var app = builder.Build();
+
+// Enable CORS
+app.UseCors("AllowAllOrigins");
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
